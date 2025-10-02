bd9509941be287a2304aaa3bf1984510ead2b644
Eason Wong <hzf510920@163.com>
2025-10-02 23:21:39 +0800

    Do the same thing using langchain.

--- 03_large_language_model/localizing_llm.py
+++ 03_large_language_model/localizing_llm.py
@@ -1,8 +1,18 @@
 #%%
 import ollama
 from pprint import pprint
+from langchain_community.llms.ollama import Ollama
 
+#%%
 response = ollama.generate("gemma2:2b", "What's an LLM")
 
 #%%
 pprint(response["response"])
+print("=======================================================")
+
+#%%
+llm = Ollama(model="gemma2:2b")
+response = llm.invoke("What is a cat")
+print(response)
+
+#%%
