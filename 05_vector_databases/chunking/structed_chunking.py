6e6637535e5b99b006463efb6ff8bdb94bdcdacc
Eason Wong <hzf510920@163.com>
2025-10-25 21:30:59 +0800

    Pot for strcture base chunking.

--- 05_vector_databases/chunking/structed_chunking.py
+++ 05_vector_databases/chunking/structed_chunking.py
@@ -3,6 +3,7 @@ from langchain.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import matplotlib.pyplot as plt
 from os import path
+import seaborn as sns
 
 #%%
 # 设置matplotlib中文显示
@@ -15,8 +16,17 @@ doc = loader.load()
 doc
 
 #%%
-splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50, separators=["\n\n", "\n", " ", ".", ",", "!", "?"])
+splitter = RecursiveCharacterTextSplitter(chunk_size=256, chunk_overlap=50, separators=["\n\n", "\n", " ", ".", ",", "!", "?"])
 chunks = splitter.split_documents(doc)
 len(chunks)
+
+#%%
+chunk_lengths = [len(chunk.page_content) for chunk in chunks]
+
+sns.histplot(chunk_lengths, bins=50, binrange=(100, 300), kde=True)
+plt.title("Chunk 长度分布图")
+plt.xlabel("Chunk 长度")
+plt.ylabel("数量")
+plt.show()
 
 #%%
