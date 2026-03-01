0a9c4b6f0de3fce1b33c9f672a63e83f56727e4f
Cliff Huang <hzf510920@163.com>
2026-03-01 19:02:53 +0800

    Error:
    Evaluating: 0%| | 0/3 [00:00<?, ?it/s]LLM returned 1 generations instead of requested 3. Proceeding with 1 generations.
    Exception raised in Job[1]: RateLimitError(Error code: 429 - {'error': {'message': 'You exceeded your current quota, please check your plan and billing details. For more information on this error, read the docs: https://platform.openai.com/docs/guides/error-codes/api-errors.', 'type': 'insufficient_quota', 'param': None, 'code': 'insufficient_quota'}})
    Evaluating: 100%|██████████| 3/3 [00:35<00:00, 11.90s/it]
    {'context_precision': 1.0000, 'answer_relevancy': nan, 'faithfulness': 0.5000}

--- c06_RAG/rag_evaluate.py
+++ c06_RAG/rag_evaluate.py
@@ -2,15 +2,15 @@
 from datasets import Dataset
 from ragas.metrics import _answer_relevancy, _context_precision, _faithfulness
 from ragas import evaluate
-from langchain_openai import OpenAI
+from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv, find_dotenv
+import os
 
 load_dotenv(find_dotenv())
 
 # %%
 my_sample = {
-    "question": "What is the capital of Germany in 1960?",
-    "answer": [ "In 1960, the capital of Germany was Bonn. East Berlin was the capital of East Germany." ],
+    "question": ["What is the capital of Germany in 1960?"],
     "contexts": [
         [
             "Berlin is the capital of Germany.", 
@@ -18,15 +18,18 @@ my_sample = {
             "Bonn was the capital of West Germany during the same period."
         ]
     ],
+    "answer": [ "In 1960, the capital of Germany was Bonn. East Berlin was the capital of East Germany." ],
     "ground_truth": ["Berlin"]
 }
 
 dataset = Dataset.from_dict(my_sample)
+# %%
+metrics = [ _context_precision, _answer_relevancy, _faithfulness ]
+metrics
 
 # %% evaluate
-model = OpenAI(model="gpt-4o-mini")
-metrics = [_answer_relevancy, _context_precision, _faithfulness]
+model = ChatOpenAI(model="openai/gpt-4o-mini", base_url="https://openrouter.ai/api/v1", api_key=os.environ.get("OPENROUTER_API_KEY"))
 res = evaluate(dataset, llm=model, metrics=metrics)
-res
+print(res)
 
 # %%
