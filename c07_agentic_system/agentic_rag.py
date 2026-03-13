0dc80588565e62c9a33b7f184628fdcd694b18db
Cliff Huang <hzf510920@163.com>
2026-03-13 23:22:38 +0800

    feat: update agentic RAG script and dependencies

--- c07_agentic_system/agentic_rag.py
+++ c07_agentic_system/agentic_rag.py
@@ -25,5 +25,35 @@ embeddings = OpenAIEmbeddings(base_url="https://openrouter.ai/api/v1", api_key=o
 
 # %%
 vectorstore = FAISS.from_documents(chunks, embeddings)
+retriever = vectorstore.as_retriever(return_similarities=True)
 
+# %%
+query = "What is relativity?"
+prompt_template = ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful assistant that answers questions about the principle of relativity.You can get contextial information from the retrieved documents. If you don't know the answer, just say 'insufficient information'. Always use the retrieved documents to answer the question."),
+    ("user", "<context>{context}</context>\n\n<question>{question}</question>")
+])
+retrieved_docs = retriever.invoke(query)
+retrieved_docs_str = "; ".join([doc.page_content for doc in retrieved_docs])
+chain = prompt_template | llm
+rag_response = chain.invoke({"context": retrieved_docs_str, "question": query})
+
+# %%
+rag_response.content
+
+# %% a simple test
+search_tool = TavilySearchResults(max_results=5, include_answer=True)
+search_response = search_tool.invoke({"query": query})
+[doc["content"] for doc in search_response]
+
+# %%
+if rag_response.content == "insufficient information":
+    print("RAG model did not have enough information. Using search tool to find the answer.")
+    search_response = search_tool.invoke({"query": query})
+    context = "; ".join([doc["content"] for doc in search_response])
+    final_response = chain.invoke({"context": context, "question": query})
+else:
+    final_response = rag_response.content
+
+final_response
 # %%
